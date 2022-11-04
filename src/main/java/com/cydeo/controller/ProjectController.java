c9fa4487bf32166f06c26e9cb41a8080086583cf
sarakara <gulcinsarankara@hotmail.com>
2022-11-04 15:38:55 -0400

    ticketing app

--- src/main/java/com/cydeo/controller/ProjectController.java
+++ src/main/java/com/cydeo/controller/ProjectController.java
@@ -1,30 +1,35 @@
 package com.cydeo.controller;
 
+import com.cydeo.dto.ProjectDTO;
+import com.cydeo.service.ProjectService;
+import com.cydeo.service.UserService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/project")
 public class ProjectController {
 
-//    private final UserService userService;
-//    private final ProjectService projectService;
-//
-//    public ProjectController(UserService userService, ProjectService projectService) {
-//        this.userService = userService;
-//        this.projectService = projectService;
-//    }
-//
-//    @GetMapping("/create")
-//    public String createProject(Model model) {
-//
-//        model.addAttribute("project", new ProjectDTO());
-//        model.addAttribute("managers", userService.findManagers());
-//        model.addAttribute("projects", projectService.findAll());
-//
-//        return "/project/create";
-//
-//    }
+    private final UserService userService;
+    private final ProjectService projectService;
+
+    public ProjectController(UserService userService, ProjectService projectService) {
+        this.userService = userService;
+        this.projectService = projectService;
+    }
+
+    @GetMapping("/create")
+    public String createProject(Model model) {
+
+        model.addAttribute("project", new ProjectDTO());
+        model.addAttribute("managers", userService.listAllByRole("manager"));
+        model.addAttribute("projects", projectService.listAllProjects());
+
+        return "/project/create";
+
+    }
 //
 //    @PostMapping("/create")
 //    public String insertProject(@Valid @ModelAttribute("project") ProjectDTO project, BindingResult bindingResult, Model model) {
